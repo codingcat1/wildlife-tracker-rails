cc34e0d3758c3e6144b10549fb934a323c02db50
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-04 16:08:14 -0700

    create routes, controllers and htmls to create new, edit, and update species.

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,30 @@
+class SpeciesController < ApplicationController
+  def index
+    @species = Species.all
+    render('species/index.html.erb')
+  end
+
+  def new
+    @species = Species.new(params[:species])
+    render('species/new.html.erb')
+  end
+
+  def create
+    @species = Species.create(params[:species])
+    index
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    render('species/edit.html.erb')
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(params[:species])
+      index
+    else
+      render('species/edit.html.erb')
+    end
+  end
+end
