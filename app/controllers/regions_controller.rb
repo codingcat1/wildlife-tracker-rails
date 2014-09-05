8298ad6431a637399d21b454dabad57f573763e8
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-05 16:57:38 -0700

    need to fix drop down menu for 'create' method for regions.

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,7 +1,7 @@
 class RegionsController < ApplicationController
 
   def create
-    @region = Region.create(params[:regions])
+    @region = Region.create(params[:region])
     redirect_to :back
   end
 
