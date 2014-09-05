eef118b6477050d8fe11fe3336ee3a02d08acaba
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-05 09:36:39 -0700

    remove sightings_species table from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904234339) do
+ActiveRecord::Schema.define(version: 20140905163434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 20140904234339) do
     t.datetime "date"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "sightings_species", force: true do |t|
-    t.integer  "species_id"
-    t.integer  "sighting_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
