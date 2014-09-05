de5bb5cb2db05c75fd22219b3409ed70dc7f3082
Ruben Rhodes & Catherine Tran <student@epicodus.com>
2014-09-05 15:51:10 -0700

    create association between regions and sightings.

--- spec/model/sighting_spec.rb
+++ spec/model/sighting_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Sighting do
   it { should belong_to :species }
+  it { should belong_to :region }
 
   it { should validate_presence_of :date }
   it { should validate_presence_of :latitude }
