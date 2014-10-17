fa5c0789cf238d0fe98a962b3ddaebdfaa6e263a
Catherine Tran <tranc489@gmail.com>
2014-10-17 16:58:31 -0700

    fix route to the homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match('/', {:via => :get, :to => 'species#index'})
   match('species', {:via => :get, :to => 'species#index'})
   match('species', {:via => :post, :to => 'species#create'})
   # match('species', {:via => :get, :to => 'species#new'})
